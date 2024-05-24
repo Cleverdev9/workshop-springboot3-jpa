b743490a9f131ac7e9e7a0aff95db85b9975ec8b
Cleverdev9 <cleversoncoelhodev9@gmail.com>
2024-05-24 06:49:50 -0300

    Project created

---
+++ src/main/java/com/crlsistemas/projwebservices/ProjwebservicesApplication.java
@@ -0,0 +1,13 @@
+package com.crlsistemas.projwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjwebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjwebservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crlsistemas/projwebservices/ProjwebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crlsistemas.projwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjwebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
