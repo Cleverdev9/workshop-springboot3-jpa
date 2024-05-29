de5d807245162cdc3077b44981b5b09eabb6bc6c
Cleverdev9 <cleversoncoelhodev9@gmail.com>
2024-05-29 12:09:16 -0300

    User delete

--- src/main/java/com/crlsistemas/projwebservices/resources/UserResource.java
+++ src/main/java/com/crlsistemas/projwebservices/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,7 +17,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.crlsistemas.projwebservices.entities.User;
 import com.crlsistemas.projwebservices.services.UserService;
 
-import jakarta.servlet.Servlet;
+
 
 @RestController
 @RequestMapping(value = "/users")
@@ -45,6 +46,12 @@ public class UserResource {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
 				.buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).body(obj);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 
 }
